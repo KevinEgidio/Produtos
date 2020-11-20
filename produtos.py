8714f1d544c6c7777d52eb239ee5fc9b0370c664
KevinEgidio <62996149+KevinEgidio@users.noreply.github.com>
2020-11-20 13:37:38 -0300

    Update produtos.py

--- produtos.py
+++ produtos.py
@@ -13,7 +13,7 @@ def biblioteca(valor):
     d = {}
     d[1] = "Papel Higiênico"
     d[2] = "Shampoo"
-    d[3] = "Condicionador"
+    d[3] = "Higiene Bocal"
     d[4] = "Maquiagens"
     d[5] = "Detergente"
     d[6] = "Desenfetante"
@@ -54,7 +54,7 @@ def biblioteca(valor):
 
 
 def produtos_1():    
-    produtos_1 = (" 1. Papel Higiênico\n 2. Shampoo\n 3. Condicionador\n 4. Maquiagens")
+    produtos_1 = (" 1. Papel Higiênico\n 2. Shampoo\n 3. Higiene Bocal\n 4. Maquiagens")
     return print(produtos_1)
 def produtos_2():
     produtos_2 = (" 5. Detergente\n 6. Desinfetante\n 7. Amaciante\n 8. Sabão em pó")
@@ -62,11 +62,12 @@ def produtos_2():
 def produtos_3():
     produtos_3 = (" 9. Cervejas\n 10. Refrigerantes\n 11. Vinhos\n 12. Sucos Naturais")
     return print(produtos_3)
+
 def produtos_4():
     produtos_4 = (" 13. Serrote\n 14. Furadeira e Pregos\n 15. Serra Circular\n 16. Lixadeira")
     return print(produtos_4)
 def produtos_5():
-    produtos_5 = (" 17. Leite\n 18. Legumes\n 19. Proteína\n 20. Grãos")
+    produtos_5 = (" 17. Leite\n 18. derivados de lactobacilos\n 19. Proteína\n 20. Grãos")
     return print(produtos_5)
 def produtos_6():
     produtos_6 = (" 21. Hamburguer\n 22. Comida Congelada\n 23. Nuggets\n 24. Massas Congeladas")
@@ -75,7 +76,7 @@ def produtos_7():
     produtos_7 = (" 25. Legumes\n 26. Verduras\n 27. Vegetais\n 28. Frutas")
     return print(produtos_7)
 def produtos_8():
-    produtos_8 = (" 29. Pães\n 30. Doces\n 31. Frios\n 32. Bebidas quentes")
+    produtos_8 = (" 29. Pães\n 30. Doces\n 31. Frios\n 32. Café")
     return print(produtos_8)
 def produtos_9():
     produtos_9 = (" 33. Carne de Boi\n 34. Carne de Porco\n 35. Carne para Churrasco\n 36. Linguiça")
@@ -199,8 +200,62 @@ def carrinho():
         if confirmação == 00:
             
             print("Obrigado pro vir ao mercado do Xinbica, Até Mais!!!")
+
+    return carrinho_compras
             
         
 
+def dicionario_coord(produto):
+    tup = {}
+    tup["Papel Higiênico"] = (0, 0)
+    tup["Shampoo"] = (0, 1)
+    tup["Higiene Bocal"] = (0, 2)
+    tup["Maquiagens"] = (0, 3)
+    tup["Detergente"] = (0, 4)
+    tup["Desinfetante"] = (0, 5)
+    tup["Amaciante"] = (0, 6)
+    tup["Sabão em pó"] = (0, 7)
+    tup["Cervejas"] = (0, 8)
+    tup["Refrigerantes"] = (0, 9) 
+    tup["Vinhos"] = (1, 0)
+    tup["Sucos Naturais"] = (1, 1)
+    tup["Serrote"] = (1, 2)
+    tup["Furadeira e Pregos"] = (1, 3)
+    tup["Serra Circular"] = (1, 4)
+    tup["Lixadeira"] = (1, 5)
+    tup["Leite"] = (1, 6)
+    tup["derivados de lactobacilos"] = (1, 7)
+    tup["Proteína"] = (1, 8)
+    tup["Grãos"] = (1, 9)
+    tup["Hamburguer"] = (2, 0)
+    tup["Comida Congelada"] = (2, 7)
+    tup["Nuggets"] = (2, 3)
+    tup["Massas Congeladas"] = (2, 4)
+    tup["Legumes"] = (2, 5)
+    tup["Verduras"] = (2, 6)
+    tup["Vegetais"] = (2, 7)
+    tup["Frutas"] = (2, 8)
+    tup["Pães"] = (2, 9) 
+    tup["Doces"] = (3, 0)
+    tup["Frios"] = (3, 1)
+    tup["Café"] = (3, 2)
+    tup["Carne de Boi"] = (3, 3)
+    tup["Carne de Porco"] = (3, 4)
+    tup["Carne para Churrasco"] = (3, 5)
+    tup["Linguiça"] = (3, 6)
+    
+    return tup[produto]
 
-carrinho()
+
+
+
+def trans_prod_em_coord():
+    prod_carrinho = carrinho()
+    lista_coord_prod = []
+
+    for i in range(len(prod_carrinho)):
+        lista_coord_prod.append(dicionario_coord(prod_carrinho[i]))
+
+    return lista_coord_prod
+
+trans_prod_em_coord()
